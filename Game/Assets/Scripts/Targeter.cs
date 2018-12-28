e96245505b11a8220409aaa5897d827dc68f751a
ElasticCracker <45251205+ElasticCracker@users.noreply.github.com>
2018-12-27 19:48:00 -0500

    test

---
+++ Game/Assets/Scripts/Targeter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeter : MonoBehaviour { 
+
+    private float counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            counter += 1;
+            if (counter == 1)
+            {
+
+            }
+            transform.SetParent(collision.transform);
+            
+            //collision.gameObject
+        }
+    }
+
+}
---
+++ Game/Assets/Scripts/TurretShooter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret: MonoBehaviour
+{
+    public float moveSpeed;
+    private bool isStuck = false;
+    void Start()
+    {
+        StartCoroutine(KillPlayer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isStuck)
+        {
+            transform.Translate(0, moveSpeed, 0);
+        }
+    }
+    IEnumerator KillPlayer()
+    {
+        yield return new WaitForSeconds(2);
+        if (!isStuck)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Circle")
+        {
+            transform.SetParent(collision.transform);
+            transform.localRotation *= Quaternion.Euler(0, 0, 180);
+            isStuck = true;
+        }
+    }
+}
