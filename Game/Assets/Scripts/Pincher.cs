4f4a0241d6195b0a4450456ad7d3a79ada48c247
ElasticCracker <45251205+ElasticCracker@users.noreply.github.com>
2019-01-01 22:11:56 -0500

    pincherstuff

---
+++ Game/Assets/Scripts/Pincher.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pincher : MonoBehaviour
+{
+    public float maxHealth;
+    public float currentHealth;
+    public float moveSpeed;
+    private bool grappled; // This bool determines if a building is being grabbed by the pincher
+    public GameObject planet; // The planet of the level
+    public GameObject target; //The thing the pincher is trying to kill
+    private GameObject childTarget; //Grabs target of pincher child
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, planet.transform.position) > 3)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, planet.transform.position, moveSpeed * Time.deltaTime);
+
+        }
+
+        PincherTargeter childTarget = GetComponentInChildren<PincherTargeter>();
+        target = childTarget.pinchtarget;
+
+        if(target != null)
+        {
+            transform.SetParent(planet.transform);
+        }
+    }
+}
---
+++ Game/Assets/Scripts/PincherTargeter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PincherTargeter : MonoBehaviour
+{
+    public GameObject pinchtarget; //target that the pincher wants to attack
+    bool looking; //determines if a target has been found
+    // Start is called before the first frame update
+    void Start()
+    {
+        looking = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && looking == true)
+        {
+            //Debug.Log("GotOne");
+            looking = false;
+            pinchtarget = collision.gameObject;
+            
+        }
+    }
+}
