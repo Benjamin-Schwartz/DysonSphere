2918b4a19cf95dbc7c5d4b38e385857bf24684a8
ihavebeenjamin <42360305+ihavebeenjamin@users.noreply.github.com>
2019-01-21 16:51:35 -0600

    Fixed asteroid Spawning

--- Game/Assets/Scripts/AsteroidSpawner.cs
+++ Game/Assets/Scripts/AsteroidSpawner.cs
@@ -16,6 +16,12 @@ public class AsteroidSpawner : MonoBehaviour {
     public float initialbuildTime;
     private EnergyBar EnergyBar;
 
+    //circular spawning
+    public int numnObjects = 10;
+    public Vector3 center;
+    
+
+
 
 
     public float spawnTime;
@@ -23,8 +29,10 @@ public class AsteroidSpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-       
-    EnergyBar = FindObjectOfType<EnergyBar>();
+         center = transform.position;
+        EnergyBar = FindObjectOfType<EnergyBar>();
+        
+
          
 }
 
@@ -35,12 +43,25 @@ public class AsteroidSpawner : MonoBehaviour {
 
         if (currentTime >= spawnTime && EnergyBar.EnergyStatus < 1)
          {
-        Instantiate(Asteroid, new Vector2(Random.Range(-xDistance, xDistance)+ 3, Random.Range(-yDistance, yDistance) + 3), Quaternion.identity);
+            //Instantiate(Asteroid, new Vector2(Random.Range(-xDistance, xDistance)+ 3, Random.Range(-yDistance, yDistance) + 3), Quaternion.identity);
+            Vector3 pos = RandomCirlce(center, 5.0f);
+           // Quaternion rot = Quaternion.FromToRotation(0,0,0);
+            Instantiate(Asteroid, pos, transform.rotation);
          currentTime = 0;
         
         }
 
    }
+    Vector3 RandomCirlce(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius + Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
+    }
+    
    
 
         }
